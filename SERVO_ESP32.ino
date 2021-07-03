9cd5822d93e718a66623f3df42d25f72bcc00115
Raphael <you@example.com>
2021-07-03 16:48:44 -0300

    New engine variables

--- SERVO_ESP32.ino
+++ SERVO_ESP32.ino
@@ -9,24 +9,35 @@ int posMyServoFirst = 0;
 Servo myservoSecond;
 int posMyServoSecond = 0;
 
+Servo myservoThird;
+int posMyServoThird = 0;
+
+Servo myservoForth;
+int posMyServoForth= 0;
+
 void setup() {
   myservoBase.attach(13);
   myservoFirst.attach(12);
   myservoSecond.attach(14);
-  
+  myservoThird.attach(27); 
+  myservoForth.attach(26);
+
   myservoBase.write(90);
-  myservoFirst.write(30);
+  myservoFirst.write(90);
   myservoSecond.write(90);
+  myservoThird.write(90);
+  myservoForth.write(120);
+  
   delay(2000);
   }
 
 void loop() {
   motor1();
   delay(1000);
-  motor2();
-  delay(1000);
   motor3();
   delay(1000);
+  motor4();
+  motor5();
 }
 
 void motor1(){
@@ -35,6 +46,9 @@ void motor1(){
     myservoBase.write(posMyServoBase);              
     delay(20);                       
   }
+  
+  delay(1000);
+  motor2();
   delay(3000);
 
   //Vai do final para o começo
@@ -60,13 +74,13 @@ void motor1(){
 }
 
 void motor2(){
-    for (posMyServoFirst = 30; posMyServoFirst <= 100; posMyServoFirst += 1) {
+    for (posMyServoFirst = 90; posMyServoFirst <= 120; posMyServoFirst += 1) {
     myservoFirst.write(posMyServoFirst);              
     delay(20);                       
   }
   delay(3000);
   
-  for (posMyServoFirst = 100; posMyServoFirst >= 30; posMyServoFirst -= 1) { 
+  for (posMyServoFirst = 120; posMyServoFirst >= 90; posMyServoFirst -= 1) { 
     myservoFirst.write(posMyServoFirst);              
     delay(20);                       
   }
@@ -82,6 +96,34 @@ void motor3(){
   
   for (posMyServoSecond = 140; posMyServoSecond >= 90; posMyServoSecond -= 1) { 
     myservoSecond.write(posMyServoSecond);              
+    delay(20);                       
+  }
+  delay(3000);
+}
+
+void motor4(){
+    for (posMyServoThird = 90; posMyServoThird <= 140; posMyServoThird += 1) {
+    myservoThird.write(posMyServoThird);              
+    delay(20);                       
+  }
+  delay(3000);
+  
+  for (posMyServoThird = 140; posMyServoThird >= 90; posMyServoThird -= 1) { 
+    myservoThird.write(posMyServoThird);              
+    delay(20);                       
+  }
+  delay(3000);
+}
+
+void motor5(){
+    for (posMyServoForth = 120; posMyServoForth <= 150; posMyServoForth += 1) {
+    myservoForth.write(posMyServoForth);              
+    delay(20);                       
+  }
+  delay(3000);
+  
+  for (posMyServoForth = 150; posMyServoForth >= 120; posMyServoForth -= 1) { 
+    myservoForth.write(posMyServoForth);              
     delay(20);                       
   }
   delay(3000);
